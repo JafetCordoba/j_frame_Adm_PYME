e3e46c8d688ea9eab6cad54fc923a34c3bfff595
Jafet Córdoba <jafetcordoba@gmail.com>
2024-04-07 18:11:16 -0600

    Modificacion de clases

--- Inventario/src/main/java/com/mycompany/inventario/Inventario.java
+++ Inventario/src/main/java/com/mycompany/inventario/Inventario.java
@@ -1,59 +1,38 @@
 package com.mycompany.inventario;
 
-public class Inventario extends Producto {
-    
-    private float valor;
-    private int cantidad;
-    private String tipo;
-    private String ubicacion;
-    private String estado;
+public class Inventario {
 
-    public Inventario(float valor, int cantidad, boolean disponibilidad, String tipo, String ubicacion, String nombre, float codigo, String estado, String pasillo) {
-        super(nombre, disponibilidad, 0, codigo, pasillo); // El precio no se pasa a super() ya que no está definido en Producto
-        this.valor = valor;
+    private Producto producto;
+    private int cantidad;
+    private String ubicacion;
+
+    public Inventario(Producto producto, int cantidad, String ubicacion) {
+        this.producto = producto;
         this.cantidad = cantidad;
-        this.tipo = tipo;
         this.ubicacion = ubicacion;
-        this.estado = estado;
     }
 
-    public float getValor() {
-        return valor;
+    public Producto getProducto() {
+        return producto;
     }
 
     public int getCantidad() {
         return cantidad;
     }
 
-    public String getTipo() {
-        return tipo;
-    }
-
     public String getUbicacion() {
         return ubicacion;
     }
 
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setValor(float valor) {
-        this.valor = valor;
+    public void setProducto(Producto producto) {
+        this.producto = producto;
     }
 
     public void setCantidad(int cantidad) {
         this.cantidad = cantidad;
     }
 
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
     public void setUbicacion(String ubicacion) {
         this.ubicacion = ubicacion;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
     }
 }
--- Inventario/src/main/java/com/mycompany/inventario/Producto.java
+++ Inventario/src/main/java/com/mycompany/inventario/Producto.java
@@ -1,58 +1,38 @@
 package com.mycompany.inventario;
 
 public class Producto {
-    
-    private String nombre;
-    private boolean disponibilidad;
-    private float precio;
-    private float codigo;
-    private String pasillo;
 
-    public Producto(String nombre, boolean disponibilidad, float precio, float codigo, String pasillo) {
+    private String nombre;
+    private String descripcion;
+    private float precio;
+
+    public Producto(String nombre, String descripcion, float precio) {
         this.nombre = nombre;
-        this.disponibilidad = disponibilidad;
+        this.descripcion = descripcion;
         this.precio = precio;
-        this.codigo = codigo;
-        this.pasillo = pasillo;
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    public boolean isDisponibilidad() {
-        return disponibilidad;
+    public String getDescripcion() {
+        return descripcion;
     }
 
     public float getPrecio() {
         return precio;
     }
 
-    public float getCodigo() {
-        return codigo;
-    }
-
-    public String getPasillo() {
-        return pasillo;
-    }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    public void setDisponibilidad(boolean disponibilidad) {
-        this.disponibilidad = disponibilidad;
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
     }
 
     public void setPrecio(float precio) {
         this.precio = precio;
-    }
-
-    public void setCodigo(float codigo) {
-        this.codigo = codigo;
-    }
-
-    public void setPasillo(String pasillo) {
-        this.pasillo = pasillo;
     }
 }
